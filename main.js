f3118742e612b3a501230bdfb3b84a1d2a814c7d
Patrick <patriick.dev@hotmail.com>
2023-09-20 21:48:26 -0300

    Feat: iniciando o desenvolvimento do front end

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["Desenvolvedor Front End", "Desenvolvedor Back End"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
