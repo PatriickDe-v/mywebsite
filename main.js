51986dd86a333cd339f59cf1edef3ea2ca0c9579
Patrick <patriick.dev@hotmail.com>
2023-09-24 19:01:43 -0300

    feat: projeto finalizado por em quanto

--- main.js
+++
@@ -1,7 +0,0 @@
-let typed = new Typed(".text", {
-    strings: ["Desenvolvedor Front End", "Desenvolvedor Back End"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
